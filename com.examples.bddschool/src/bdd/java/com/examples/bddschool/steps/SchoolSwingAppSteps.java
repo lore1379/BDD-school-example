4ae07eb78fd57339c55b04db2628b8833de9a569
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-19 16:46:07 +0200

    Refactor

--- com.examples.bddschool/src/bdd/java/com/examples/bddschool/steps/SchoolSwingAppSteps.java
+++ com.examples.bddschool/src/bdd/java/com/examples/bddschool/steps/SchoolSwingAppSteps.java
@@ -5,32 +5,43 @@ import org.bson.Document;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class SchoolSwingAppSteps {
+
+	static final String DB_NAME = "test-db";
+	static final String COLLECTION_NAME = "test-collection";
+
+	private static int mongoPort = 
+			Integer.parseInt(System.getProperty("mongo.port", "27017"));
+	
+	static final String STUDENT_FIXTURE_2_NAME = "second student";
+	static final String STUDENT_FIXTURE_2_ID = "2";
+	static final String STUDENT_FIXTURE_1_NAME = "first student";
+	static final String STUDENT_FIXTURE_1_ID = "1";
+	
+	private MongoClient mongoClient;
+
+	@Before
+	public void setup() {		
+		mongoClient = new MongoClient(
+				new ServerAddress("localhost", mongoPort));
+		mongoClient.getDatabase(DB_NAME).drop();
+	}
+	
+	@After
+	public void tearDown() {
+		mongoClient.close();
+	}
 	
 	@Given("The database contains a few students")
 	public void the_database_contains_a_few_students() {
-	    MongoClient mongoClient = new MongoClient(
-	    		new ServerAddress("localhost", Integer.parseInt(System.getProperty("mongo.port", "27017"))));
-	    mongoClient.getDatabase("test-db").drop();
-	    mongoClient
-			.getDatabase("test-db")
-			.getCollection("test-collection")
-			.insertOne(
-				new Document()
-					.append("id", "1")
-					.append("name", "first student"));
-	    mongoClient
-			.getDatabase("test-db")
-			.getCollection("test-collection")
-			.insertOne(
-				new Document()
-					.append("id", "2")
-					.append("name", "second student"));
-	    mongoClient.close();
+	    addStudentToDatabase(STUDENT_FIXTURE_1_ID, STUDENT_FIXTURE_1_NAME);
+	    addStudentToDatabase(STUDENT_FIXTURE_2_ID, STUDENT_FIXTURE_2_NAME);
 	}
 
 	@Given("The Student View is shown")
@@ -57,4 +68,13 @@ public class SchoolSwingAppSteps {
 	    throw new io.cucumber.java.PendingException();
 	}
 
+	private void addStudentToDatabase(String id, String name) {
+		mongoClient
+		.getDatabase(DB_NAME)
+		.getCollection(COLLECTION_NAME)
+		.insertOne(
+				new Document()
+				.append("id", id)
+				.append("name", name));
+	}
 }
