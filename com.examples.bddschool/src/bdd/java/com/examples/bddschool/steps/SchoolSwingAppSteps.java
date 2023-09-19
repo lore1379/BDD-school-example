16d183aa6448600c8a817862d76c80eefe2d6799
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-19 16:35:25 +0200

    Implemented database contains a few students

--- com.examples.bddschool/src/bdd/java/com/examples/bddschool/steps/SchoolSwingAppSteps.java
+++ com.examples.bddschool/src/bdd/java/com/examples/bddschool/steps/SchoolSwingAppSteps.java
@@ -1,5 +1,10 @@
 package com.examples.bddschool.steps;
 
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -8,8 +13,24 @@ public class SchoolSwingAppSteps {
 	
 	@Given("The database contains a few students")
 	public void the_database_contains_a_few_students() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    MongoClient mongoClient = new MongoClient(
+	    		new ServerAddress("localhost", Integer.parseInt(System.getProperty("mongo.port", "27017"))));
+	    mongoClient.getDatabase("test-db").drop();
+	    mongoClient
+			.getDatabase("test-db")
+			.getCollection("test-collection")
+			.insertOne(
+				new Document()
+					.append("id", "1")
+					.append("name", "first student"));
+	    mongoClient
+			.getDatabase("test-db")
+			.getCollection("test-collection")
+			.insertOne(
+				new Document()
+					.append("id", "2")
+					.append("name", "second student"));
+	    mongoClient.close();
 	}
 
 	@Given("The Student View is shown")
