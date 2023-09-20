bc83d84d5f21943b6a01efba12099541a935df83
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-20 10:38:20 +0200

    Implemented id and name text boxes

--- com.examples.bddschool/src/main/java/com/examples/bddschool/view/swing/StudentSwingView.java
+++ com.examples.bddschool/src/main/java/com/examples/bddschool/view/swing/StudentSwingView.java
@@ -1,6 +1,5 @@
 package com.examples.bddschool.view.swing;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -8,6 +7,11 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import com.examples.bddschool.controller.SchoolController;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
 
 public class StudentSwingView extends JFrame {
 
@@ -16,7 +20,14 @@ public class StudentSwingView extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtId;
+	private JTextField txtName;
 
+	public void setSchoolController(SchoolController schoolController) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	/**
 	 * Launch the application.
 	 */
@@ -41,14 +52,51 @@ public class StudentSwingView extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setName("nameTextBox");
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblId = new JLabel("id");
+		GridBagConstraints gbc_lblId = new GridBagConstraints();
+		gbc_lblId.insets = new Insets(0, 0, 5, 5);
+		gbc_lblId.anchor = GridBagConstraints.EAST;
+		gbc_lblId.gridx = 0;
+		gbc_lblId.gridy = 0;
+		contentPane.add(lblId, gbc_lblId);
+		
+		txtId = new JTextField();
+		txtId.setName("idTextBox");
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		contentPane.add(txtId, gbc_textField);
+		txtId.setColumns(10);
+		
+		JLabel lblName = new JLabel("name");
+		GridBagConstraints gbc_lblName = new GridBagConstraints();
+		gbc_lblName.anchor = GridBagConstraints.EAST;
+		gbc_lblName.insets = new Insets(0, 0, 0, 5);
+		gbc_lblName.gridx = 0;
+		gbc_lblName.gridy = 1;
+		contentPane.add(lblName, gbc_lblName);
+		
+		txtName = new JTextField();
+		txtName.setName("nameTextBox");
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 1;
+		contentPane.add(txtName, gbc_textField_1);
+		txtName.setColumns(10);
 	}
 
-	public void setSchoolController(SchoolController schoolController) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
