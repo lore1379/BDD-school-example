e4f345e8593bc650a26a77d1ff81b92536a9359e
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-20 11:55:58 +0200

    Revert "Test Controller's allStudents"
    This reverts commit a8dbfaa4fb2dd536d14ca48fb2cd79bb359385d0.

--- com.examples.bddschool/src/main/java/com/examples/bddschool/repository/StudentRepository.java
+++ com.examples.bddschool/src/main/java/com/examples/bddschool/repository/StudentRepository.java
@@ -1,11 +1,5 @@
 package com.examples.bddschool.repository;
 
-import java.util.List;
-
-import com.examples.bddschool.model.Student;
-
 public interface StudentRepository {
-
-	public List<Student> findAll();
 
 }
--- com.examples.bddschool/src/main/java/com/examples/bddschool/repository/mongo/StudentMongoRepository.java
+++ com.examples.bddschool/src/main/java/com/examples/bddschool/repository/mongo/StudentMongoRepository.java
@@ -1,8 +1,5 @@
 package com.examples.bddschool.repository.mongo;
 
-import java.util.List;
-
-import com.examples.bddschool.model.Student;
 import com.examples.bddschool.repository.StudentRepository;
 import com.mongodb.MongoClient;
 
@@ -10,12 +7,6 @@ public class StudentMongoRepository implements StudentRepository{
 
 	public StudentMongoRepository(MongoClient mongoClient, String string, String string2) {
 		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public List<Student> findAll() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
