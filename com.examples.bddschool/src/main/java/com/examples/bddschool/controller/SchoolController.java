9ef1c5ff56b37ba2403a4a2d48b8ccc4a012186b
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-20 09:34:18 +0200

    SchoolController created

---
+++ com.examples.bddschool/src/main/java/com/examples/bddschool/controller/SchoolController.java
@@ -0,0 +1,17 @@
+package com.examples.bddschool.controller;
+
+import com.examples.bddschool.repository.mongo.StudentMongoRepository;
+import com.examples.bddschool.view.swing.StudentSwingView;
+
+public class SchoolController {
+
+	public SchoolController(StudentSwingView studentView, StudentMongoRepository studentRepository) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void allStudents() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
