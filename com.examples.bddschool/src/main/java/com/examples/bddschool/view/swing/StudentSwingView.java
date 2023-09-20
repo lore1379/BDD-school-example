d1e3008fae93eb2c4baf0488884f019ac383df28
lorenzo <lorenzo.giannella@stud.unifi.it>
2023-09-20 10:49:54 +0200

    Implemented Add button initial state

--- com.examples.bddschool/src/main/java/com/examples/bddschool/view/swing/StudentSwingView.java
+++ com.examples.bddschool/src/main/java/com/examples/bddschool/view/swing/StudentSwingView.java
@@ -12,6 +12,7 @@ import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import javax.swing.JTextField;
 import java.awt.Insets;
+import javax.swing.JButton;
 
 public class StudentSwingView extends JFrame {
 
@@ -22,6 +23,7 @@ public class StudentSwingView extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtId;
 	private JTextField txtName;
+	private JButton btnAdd;
 
 	public void setSchoolController(SchoolController schoolController) {
 		// TODO Auto-generated method stub
@@ -57,9 +59,9 @@ public class StudentSwingView extends JFrame {
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0};
 		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JLabel lblId = new JLabel("id");
@@ -83,7 +85,7 @@ public class StudentSwingView extends JFrame {
 		JLabel lblName = new JLabel("name");
 		GridBagConstraints gbc_lblName = new GridBagConstraints();
 		gbc_lblName.anchor = GridBagConstraints.EAST;
-		gbc_lblName.insets = new Insets(0, 0, 0, 5);
+		gbc_lblName.insets = new Insets(0, 0, 5, 5);
 		gbc_lblName.gridx = 0;
 		gbc_lblName.gridy = 1;
 		contentPane.add(lblName, gbc_lblName);
@@ -91,11 +93,20 @@ public class StudentSwingView extends JFrame {
 		txtName = new JTextField();
 		txtName.setName("nameTextBox");
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_1.gridx = 1;
 		gbc_textField_1.gridy = 1;
 		contentPane.add(txtName, gbc_textField_1);
 		txtName.setColumns(10);
+		
+		btnAdd = new JButton("Add");
+		btnAdd.setEnabled(false);
+		GridBagConstraints gbc_btnAdd = new GridBagConstraints();
+		gbc_btnAdd.gridwidth = 2;
+		gbc_btnAdd.gridx = 0;
+		gbc_btnAdd.gridy = 2;
+		contentPane.add(btnAdd, gbc_btnAdd);
 	}
 
 
